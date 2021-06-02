34416d2d4b9813cf104ba6f379fe66ac4122d35c
Simran Gangwani <simran_gangwani@Simrans-MacBook-Pro.local>
2021-06-02 09:48:44 +0530

    Added wallis() and monte_carlo() in estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,39 @@
 import math
 import unittest
+from functools import reduce
+import random
 
+
+def wallis(n):
+    i = 1
+    numerator = 1
+    denomenator = 1
+    for i in range(n+1):
+        x = 4 * i * i
+        numerator *= x
+        denomenator *= (x-1)
+    print(2 * (numerator/denomenator))
+    return (2 * (numerator/denomenator))
+
+def monte_carlo(n):
+    circle = 0
+    sq = 0
+    
+    for i in range(1, n+1):
+        # So as to cover all the 4 quadrants of square range = (-1,1)
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)
+        
+        #assuming center of the circle as origin
+        dist = x*x + y*y
+        
+        #since the radius of circle = 1(given) all the points lying inside the sircle should have distance less than equals to one
+        if(dist <= 1):
+            circle += 1
+        square += 1
+        
+        return(4* (circle/square))
+    
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
@@ -11,6 +44,8 @@ class TestWallis(unittest.TestCase):
         for i in range(500, 600):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+    
+    
 
 
 class TestMC(unittest.TestCase):
